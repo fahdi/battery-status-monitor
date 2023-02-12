6c535e291e0f1a51d72b8fa30c47dbcf0401364c
Fahad Murtaza <434134+fahdi@users.noreply.github.com>
2023-02-12 14:13:23 +0500

    Added Server side code for API and introduced new node modules

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const firebase = require("firebase");
 
 // Connect to MongoDB
 mongoose.connect("mongodb://localhost:27017/battery", {
   useNewUrlParser: true,
   useUnifiedTopology: true
+});
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  databaseURL: "your-database-url",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id"
 });
 
 const batterySchema = new mongoose.Schema({
@@ -25,16 +36,31 @@ const app = express();
 app.use(express.json());
 
 app.post("/battery", async (req, res) => {
+  let statusMongoDb;
+  let statusFirebase;
   const battery = new Battery({
     charging: req.body.charging
   });
 
   try {
     const savedBattery = await battery.save();
-    res.status(201).send(savedBattery);
+    statusMongoDb = true;
   } catch (error) {
     res.status(400).send(error);
   }
+
+  try {
+    const battery = {
+      charging: req.body.charging,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    };
+    await db.collection("battery").add(battery);
+    statusFirebase = true;
+  } catch (error) {
+    res.status(400).send(error);
+  }
+  if(statusMongoDb && statusFirebase )
+    res.status(201).send(savedBattery);
 });
 
 app.listen(3000, () => {
