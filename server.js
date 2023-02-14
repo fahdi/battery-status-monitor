6387f087ff5666508c44dd3c65505e26fd729a2f
Fahad Murtaza <434134+fahdi@users.noreply.github.com>
2023-02-14 23:13:41 +0500

    fixed node espress based server =.js

--- server.js
+++ server.js
@@ -1,68 +1,81 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const firebase = require("firebase");
+const express  = require('express');
+const mongoose = require('mongoose');
+const firebase = require('firebase');
+const jsonParser = require('json-parser');
+const PORT = 3500;
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/battery", {
+mongoose.connect('mongodb://localhost:27017/battery-status', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
-// Initialize Firebase
-firebase.initializeApp({
-  apiKey: "your-api-key",
-  authDomain: "your-auth-domain",
-  databaseURL: "your-database-url",
-  projectId: "your-project-id",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-messaging-sender-id"
-});
+// // Initialize Firebase
+// firebase.initializeApp({
+//   apiKey: "your-api-key",
+//   authDomain: "your-auth-domain",
+//   databaseURL: "your-database-url",
+//   projectId: "your-project-id",
+//   storageBucket: "your-storage-bucket",
+//   messagingSenderId: "your-messaging-sender-id"
+// });
 
 const batterySchema = new mongoose.Schema({
   charging: {
     type: Boolean,
-    required: true
+    required: true,
   },
   timestamp: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-const Battery = mongoose.model("Battery", batterySchema);
+const Battery = mongoose.model('Battery', batterySchema);
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/battery", async (req, res) => {
-  let statusMongoDb;
-  let statusFirebase;
-  const battery = new Battery({
-    charging: req.body.charging
+app.post('/battery', async (req, res) => {
+
+  let statusMongoDb = false;
+  let statusFirebase = true;
+  const battery      = new Battery({
+    charging: req.body.charging,
   });
 
   try {
     const savedBattery = await battery.save();
-    statusMongoDb = true;
-  } catch (error) {
+    statusMongoDb      = true;
+  }
+  catch (error) {
     res.status(400).send(error);
   }
 
-  try {
-    const battery = {
-      charging: req.body.charging,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
-    };
-    await db.collection("battery").add(battery);
-    statusFirebase = true;
-  } catch (error) {
-    res.status(400).send(error);
+  // try {
+  //   const battery = {
+  //     charging: req.body.charging,
+  //     timestamp: firebase.firestore.FieldValue.serverTimestamp()
+  //   };
+  //   await db.collection("battery").add(battery);
+  //   statusFirebase = true;
+  // } catch (error) {
+  //   res.status(400).send(error);
+  // }
+  if (statusMongoDb && statusFirebase) {
+    res.status(201).send({'updated': true});
+  } else {
+    res.status(500).send({'500': true});
   }
-  if(statusMongoDb && statusFirebase )
-    res.status(201).send(savedBattery);
+
 });
 
-app.listen(3000, () => {
-  console.log("Battery status monitor API running on port 3000");
+app.get('/status', async (req, res) => {
+  res.status(201).send({'up': true});
+});
+
+
+app.listen(PORT, () => {
+  console.log('Battery status monitor API running on port ' + PORT);
 });
